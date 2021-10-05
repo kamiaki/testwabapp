91a1ff56c5160740f2ae7375bbea9f896329938b
aki <247557768@qq.com>
2021-10-05 12:42:20 +0800

    123

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './index.css'
 
 import routerIndex from './router/routerValues'  // 引入路由对象实例
 import vuexValues from './vuex/vuexValues'  // 引入vuex
+import 'animate.css'
 
 const app = createApp(App)
 app.use(routerIndex)
