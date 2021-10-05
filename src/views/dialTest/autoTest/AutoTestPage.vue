03008285f53d04ac5af1a4f70dad3fe58a88d6a3
aki <247557768@qq.com>
2021-10-05 20:55:25 +0800

    123

--- src/views/dialTest/autoTest/AutoTestPage.vue
+++ src/views/dialTest/autoTest/AutoTestPage.vue
@@ -5,17 +5,19 @@
     <div>{{`计算属性1: ${ppp.value1}`}}</div>
     <div>{{`计算属性2: ${ppp.value2}`}}</div>
     <div>{{`计算属性 总: ${ppp.fullName}`}}</div>
+    <div>{{`num : ${num}`}}</div>
     <div v-for="(val) in list" :key="val"> {{ val }}</div>
 </template>
 
 <script>
-    import {ref,reactive, onMounted, onBeforeUnmount, computed} from 'vue'
+    import {ref, reactive, watch, onMounted, onBeforeUnmount, computed} from 'vue'
 
     export default {
         name: "AutoTestPage",
         props: ['msg'],
         emits: ['hello'],
         setup(prop, context) {
+            let num = ref(123)
             let ppp = reactive({
                 value1: 'dsadsadsa',
                 value2: 2321321321
@@ -25,8 +27,21 @@
                 list.push(i)
             }
 
-            ppp.fullName = computed(()=>{
+            ppp.fullName = computed(() => {
                 return ppp.value1 + '-' + ppp.value2
+            })
+
+            watch(ppp, ()=> {
+                console.info(`ppp改变了`)
+            })
+            watch(num, (n, o)=> {
+                console.info(`num改变了:旧:${o}新:${n}`)
+            })
+
+            watch(()=>ppp.value1, (n, o)=> {
+                console.info(n)
+                console.info(o)
+                console.info(`ppp value1 改变了`)
             })
 
             onMounted(() => {
@@ -40,7 +55,12 @@
                 context.emit('hello', 666)
             }
 
-            return {list, danji, ppp}
+            setTimeout(() => {
+                ppp.value1 = '啊啊啊'
+                num.value = 4324324
+            }, 2000)
+
+            return {list, danji, ppp, num}
         }
     }
 </script>
