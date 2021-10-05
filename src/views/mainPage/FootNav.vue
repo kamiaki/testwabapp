5041f4f62e8e22f9a45675ce36b3bff9671c43cd
aki <247557768@qq.com>
2021-10-05 19:29:51 +0800

    123

--- src/views/mainPage/FootNav.vue
+++ src/views/mainPage/FootNav.vue
@@ -2,21 +2,36 @@
     <van-tabbar
             v-model="active"
             :placeholder="true"
+            @change="change"
     >
-        <van-tabbar-item icon="home-o">拨测</van-tabbar-item>
-        <van-tabbar-item icon="search">查询</van-tabbar-item>
-        <van-tabbar-item icon="friends-o">更多</van-tabbar-item>
+        <van-tabbar-item icon="home-o" name="拨测">拨测</van-tabbar-item>
+        <van-tabbar-item icon="search" name="查询">查询</van-tabbar-item>
+        <van-tabbar-item icon="friends-o" name="更多">更多</van-tabbar-item>
     </van-tabbar>
 </template>
 
 <script>
     import {ref} from 'vue';
+    import {useRouter} from 'vue-router'
 
     export default {
         name: "FootNav",
         setup() {
-            const active = ref(0);
-            return {active};
+            const router = useRouter()
+            const active = ref('拨测');
+
+            function change(active) {
+                switch (active) {
+                    case '拨测':
+                        router.push({name: 'DialTestPage'})
+                        break;
+                    case '查询':
+                        router.push({path: '/MainPage/QueryPage'})
+                        break;
+                }
+            }
+
+            return {active, change};
         }
     }
 </script>
