831cd394ad2b6cbe0fdd8b92ff4695909aa2bf90
aki <247557768@qq.com>
2021-10-06 14:42:25 +0800

    123

--- src/views/query/queryTest/QueryTestPage.vue
+++ src/views/query/queryTest/QueryTestPage.vue
@@ -1,10 +1,18 @@
 <template>
     <div>查询拨测结果页面</div>
+    <FormQueryTest></FormQueryTest>
+    <TableQueryTest></TableQueryTest>
 </template>
 
 <script>
+    import FormQueryTest from '/@/views/query/queryTest/FormQueryTest.vue'
+    import TableQueryTest from '/@/views/query/queryTest/TableQueryTest.vue'
+
     export default {
-        name: "QueryTestPage"
+        name: "QueryTestPage",
+        components: {
+            FormQueryTest, TableQueryTest
+        }
     }
 </script>
 
