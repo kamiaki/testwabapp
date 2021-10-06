31892f513e63238f5cc62ca2f0be111e865c2853
aki <247557768@qq.com>
2021-10-06 14:25:25 +0800

    123

--- src/views/dialTest/autoTest/AutoTestPage.vue
+++ src/views/dialTest/autoTest/AutoTestPage.vue
@@ -1,18 +1,38 @@
 <template>
-    <SwipeAutoTest></SwipeAutoTest>
-    <LampAutoTest></LampAutoTest>
-    <NotifyAutoTest></NotifyAutoTest>
+    <van-pull-refresh v-model="loading" @refresh="onRefresh">
+        <p>刷新次数: {{ count }}</p>
+        <SwipeAutoTest></SwipeAutoTest>
+        <LampAutoTest></LampAutoTest>
+        <NotifyAutoTest></NotifyAutoTest>
+    </van-pull-refresh>
 </template>
 
 <script>
     import {ref, reactive} from 'vue'
+    import { Toast } from 'vant'
     import SwipeAutoTest from "/@/views/dialTest/autoTest/SwipeAutoTest.vue";
     import LampAutoTest from "/@/views/dialTest/autoTest/LampAutoTest.vue";
     import NotifyAutoTest from "/@/views/dialTest/autoTest/NotifyAutoTest.vue";
 
     export default {
         name: "AutoTestPage",
-        components: {SwipeAutoTest, LampAutoTest, NotifyAutoTest}
+        components: {SwipeAutoTest, LampAutoTest, NotifyAutoTest},
+        setup() {
+            const count = ref(0);
+            const loading = ref(false);
+            const onRefresh = () => {
+                setTimeout(() => {
+                    Toast('刷新成功');
+                    loading.value = false;
+                    count.value++;
+                }, 1000);
+            };
+            return {
+                count,
+                loading,
+                onRefresh,
+            };
+        }
     }
 </script>
 
--- src/views/dialTest/manualTest/ManualTestPage.vue
+++ src/views/dialTest/manualTest/ManualTestPage.vue
@@ -1,15 +1,39 @@
 <template>
     <FormManualTest></FormManualTest>
-    <ContentManualTest></ContentManualTest>
+    <van-pull-refresh v-model="loading" @refresh="onRefresh">
+        <p>刷新次数: {{ count }}</p>
+        <SwipeManualTest></SwipeManualTest>
+        <TableManualTest></TableManualTest>
+    </van-pull-refresh>
 </template>
 
 <script>
+    import { ref } from 'vue';
+    import { Toast } from 'vant';
     import FormManualTest from '/@/views/dialTest/manualTest/FormManualTest.vue'
-    import ContentManualTest from '/@/views/dialTest/manualTest/ContentManualTest.vue'
+    import SwipeManualTest from '/@/views/dialTest/manualTest/SwipeManualTest.vue'
+    import TableManualTest from '/@/views/dialTest/manualTest/TableManualTest.vue'
 
     export default {
         name: "ManualTestPage",
-        components: {FormManualTest, ContentManualTest}
+        components: {FormManualTest, SwipeManualTest, TableManualTest},
+        setup() {
+            const count = ref(0);
+            const loading = ref(false);
+            const onRefresh = () => {
+                setTimeout(() => {
+                    Toast('刷新成功');
+                    loading.value = false;
+                    count.value++;
+                }, 1000);
+            };
+
+            return {
+                count,
+                loading,
+                onRefresh,
+            };
+        }
     }
 </script>
 
