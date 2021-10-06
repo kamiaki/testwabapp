cfebb5aa4d0bb865279373dd8488e96e427bc648
aki <247557768@qq.com>
2021-10-06 14:35:44 +0800

    123

--- src/views/dialTest/autoTest/LampAutoTest.vue
+++ src/views/dialTest/autoTest/LampAutoTest.vue
@@ -1,9 +1,9 @@
 <template>
     <van-grid>
-        <van-grid-item icon="photo-o" text="移动" />
-        <van-grid-item icon="photo-o" text="联通" />
-        <van-grid-item icon="photo-o" text="电信" />
-        <van-grid-item icon="photo-o" text="域名" />
+        <van-grid-item icon="photo-o" text="移动"/>
+        <van-grid-item icon="photo-o" text="联通"/>
+        <van-grid-item icon="photo-o" text="电信"/>
+        <van-grid-item icon="photo-o" text="域名"/>
     </van-grid>
 </template>
 
