131519b79e680279d682a0b056c9f5ed126617b0
aki <247557768@qq.com>
2021-10-09 16:52:05 +0800

    123

--- src/views/dialTest/manualTest/TableManualTest.vue
+++ src/views/dialTest/manualTest/TableManualTest.vue
@@ -1,22 +1,28 @@
 <template>
     <div>
-        <div>参数展示</div>
-        <van-row>
-            <van-col span="12">线程数: {{manualTestData.form.threadCount}} | 测试量: {{manualTestData.form.testCount}}</van-col>
-            <van-col span="12">测试时间: {{'2021-10-08'}}</van-col>
+        <div style="text-align: center;font-size: 25px;">参数展示</div>
+        <van-row style="margin: 5px 0 20px">
+            <van-col offset="2" span="10">
+                <div>线程数: {{manualTestData.form.threadCount}} | 测试量: {{manualTestData.form.testCount}}</div>
+            </van-col>
+            <van-col span="10">
+                <div style="text-align: right">测试时间: {{'2021-10-08'}}</div>
+            </van-col>
         </van-row>
-        <van-row>
-            <van-col span="6">网络类型</van-col>
-            <van-col span="6">最大值(ms)</van-col>
-            <van-col span="6">最小值(ms)</van-col>
-            <van-col span="6">网络波动级别</van-col>
-        </van-row>
-        <van-row v-for="item in manualTestData.tableData" :key="item">
-            <van-col span="6">{{item.name}}</van-col>
-            <van-col span="6">{{item.max}}</van-col>
-            <van-col span="6">{{item.min}}</van-col>
-            <van-col span="6">{{item.level}}</van-col>
-        </van-row>
+       <tableW>
+           <van-row class="tableH">
+               <van-col span="6">网络类型</van-col>
+               <van-col span="6">最大值(ms)</van-col>
+               <van-col span="6">最小值(ms)</van-col>
+               <van-col span="6">网络波动级别</van-col>
+           </van-row>
+           <van-row v-for="item in manualTestData.tableData" :key="item" class="tableC">
+               <van-col span="6">{{item.name}}</van-col>
+               <van-col span="6">{{item.max}}</van-col>
+               <van-col span="6">{{item.min}}</van-col>
+               <van-col span="6">{{item.level}}</van-col>
+           </van-row>
+       </tableW>
     </div>
 </template>
 
@@ -32,6 +38,19 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+    tableW{
+        .van-col {
+            text-align: center;
+            border: 1px solid #000;
+        }
+        .tableC{
+            .van-col {
+                background: #f3fffe;
+            }
+        }
+        .tableH{
+            background: #f7f7ff;
+        }
+    }
 </style>
