acd1d02c6c5131c81439bc956c3278e7f1efc938
15901173372 <247557768@qq.com>
2021-10-10 21:49:20 +0800

    123

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// import Qianming from './qianming'
+// Qianming()
 import {createApp} from 'vue'
 import App from './App.vue'
 import './index.css'
@@ -9,11 +11,14 @@ import 'animate.css'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import { ConfigProvider } from 'vant'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(routerIndex)
 app.use(vuexValues)
 app.use(Vant)
 app.use(ConfigProvider)
+app.use(ElementPlus)
 
 app.mount('#aki_app')
