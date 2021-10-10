9ad82b5da233a0a729cdc88a28e88d6692d29575
15901173372 <247557768@qq.com>
2021-10-10 22:28:46 +0800

    123

--- src/views/dialTest/manualTest/TableManualTest.vue
+++ src/views/dialTest/manualTest/TableManualTest.vue
@@ -7,20 +7,31 @@
                 <div>测试时间: {{'2021-10-08'}}</div>
             </van-col>
         </van-row>
-       <div class="tableW">
-           <van-row class="tableH">
-               <van-col span="6">网络类型</van-col>
-               <van-col span="6">最大值(ms)</van-col>
-               <van-col span="6">最小值(ms)</van-col>
-               <van-col span="6">网络波动级别</van-col>
-           </van-row>
-           <van-row v-for="item in manualTestData.tableData" :key="item" class="tableC">
-               <van-col span="6">{{item.name}}</van-col>
-               <van-col span="6">{{item.max}}</van-col>
-               <van-col span="6">{{item.min}}</van-col>
-               <van-col span="6">{{item.level}}</van-col>
-           </van-row>
-       </div>
+        <div class="tableW">
+            <van-row class="tableH">
+                <van-col span="6">网络类型</van-col>
+                <van-col span="6">最大值(ms)</van-col>
+                <van-col span="6">最小值(ms)</van-col>
+                <van-col span="6">网络波动级别</van-col>
+            </van-row>
+            <van-row v-for="item in manualTestData.tableData" :key="item" class="tableC">
+                <van-col span="6">{{item.name}}</van-col>
+                <van-col span="6">{{item.max}}</van-col>
+                <van-col span="6">{{item.min}}</van-col>
+                <van-col span="6">{{item.level}}</van-col>
+            </van-row>
+        </div>
+
+
+        <div class="table-wrapper-style1">
+            <el-table :data="tableData" style="width: 100%">
+                <el-table-column prop="date" label="Date" width="180"/>
+                <el-table-column prop="name" label="Name" width="180"/>
+                <el-table-column prop="address" label="Address"/>
+            </el-table>
+        </div>
+
+
     </div>
 </template>
 
@@ -28,6 +39,32 @@
     export default {
         name: "TableManualTest",
         props: ['manualTestData'],
+        data() {
+            return {
+                tableData: [
+                    {
+                        date: '2016-05-03',
+                        name: 'Tom',
+                        address: 'aaa',
+                    },
+                    {
+                        date: '2016-05-02',
+                        name: 'Tom',
+                        address: 'aaa',
+                    },
+                    {
+                        date: '2016-05-04',
+                        name: 'Tom',
+                        address: 'aaa',
+                    },
+                    {
+                        date: '2016-05-01',
+                        name: 'Tom',
+                        address: 'aaa',
+                    },
+                ],
+            }
+        },
         setup(props) {
             return {
                 manualTestData: props.manualTestData
@@ -37,18 +74,22 @@
 </script>
 
 <style scoped lang="less">
-    .tableW{
+    @import "src/table1Less";
+
+    .tableW {
         .van-col {
             text-align: center;
             border: 1px solid #000;
             font-size: 12px;
         }
-        .tableC{
+
+        .tableC {
             .van-col {
                 background: #f3fffe;
             }
         }
-        .tableH{
+
+        .tableH {
             background: #f7f7ff;
         }
     }
