29ec9448a224abd920f6bcb17f4afcfa98c1efa6
aki <247557768@qq.com>
2021-10-12 17:11:10 +0800

    123

--- src/api/apiAuth.js
+++ src/api/apiAuth.js
@@ -2,20 +2,25 @@ import {get, post} from '/@/utils/ajax/request'
 import authorizationUtil from "/@/utils/authorizationUtil";
 import {Toast} from 'vant'
 
+// admin => 98,101,110,106,111
+// 'Kpd4@B2IHpHK96cs' + 'guotouyingxin' => CDD979CECA4A2741EC1318FE7AF7EC56
+// https://www.cmd5.com/
 // 读取大标题数据
 const getToken = () => post('/user/v1/login', null,
     {
         params: null, showLoading: true, headers: {
             'userName': authorizationUtil.encryption('admin'),
-            'passWord': '915d141859745d44a6011c5f30ddbe4a',
+            'passWord': 'CDD979CECA4A2741EC1318FE7AF7EC56',
         }
     }).then(result => {
+    console.info(authorizationUtil.encryption('admin'))
     if (result.success) {
         return Promise.resolve(result.token)
     } else {
         return Promise.reject(`error: ${result.message}`)
     }
 }).catch((e) => {
+    console.info(e)
     Toast(`获取token异常${e}`)
 })
 
