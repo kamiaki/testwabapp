f0c1dbb3bca0ce596ac2d9d3dea9db8053b8e9ef
aki <247557768@qq.com>
2021-10-13 19:30:58 +0800

    123

--- src/views/dialTest/manualTest/ManualTestPage.vue
+++ src/views/dialTest/manualTest/ManualTestPage.vue
@@ -15,82 +15,27 @@
 </template>
 
 <script>
-    import {ref, reactive, watch, computed} from 'vue'
-    import { Toast } from 'vant';
+    import {ref} from 'vue'
     import FormManualTest from '/@/views/dialTest/manualTest/FormManualTest.vue'
     import SwipeManualTest from '/@/views/dialTest/manualTest/swipe/SwipeManualTest.vue'
     import TableManualTest from '/@/views/dialTest/manualTest/TableManualTest.vue'
-    import utils from 'aki_js_utils'
     import huaji from '/@/assets/other/huaji.png'
+    import useManualTestDefault from './useManualTestDefault'
 
     export default {
         name: "ManualTestPage",
         components: {FormManualTest, SwipeManualTest, TableManualTest},
         setup() {
             const loading = ref(false);
-            // form
-            const form = {
-                threadCount: 1,
-                testCount: 2
-            }
-            // 图表信息
-            const echartData = {
-                barV: {
-                    dataX: [utils.randomFlow(0, 30, 0),
-                        utils.randomFlow(0, 30, 0),
-                        utils.randomFlow(0, 30, 0),
-                        utils.randomFlow(0, 30, 0)]
-                },
-                barH: {
-                    dataX: [utils.randomFlow(0, 30, 0),
-                        utils.randomFlow(0, 30, 0),
-                        utils.randomFlow(0, 30, 0),
-                        utils.randomFlow(0, 30, 0)]
-                },
-            }
-            // 表格
-            const tableData = [
-                {name: '移动网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
-                {name: '联通网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
-                {name: '电信网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
-                {name: '域名网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
-            ]
-            const manualTestData = reactive({
-                form: form,
-                echartData: echartData,
-                tableData: tableData
-            })
-
-
-            // 测试刷新数据
-            function setEchartsData() {
-                manualTestData.echartData.barV.dataX = [utils.randomFlow(0, 30, 0),
-                    utils.randomFlow(0, 30, 0),
-                    utils.randomFlow(0, 30, 0),
-                    utils.randomFlow(0, 30, 0)]
-                manualTestData.echartData.barH.dataX = [utils.randomFlow(0, manualTestData.form.testCount, 0),
-                    utils.randomFlow(0, manualTestData.form.testCount, 0),
-                    utils.randomFlow(0, manualTestData.form.testCount, 0),
-                    utils.randomFlow(0, manualTestData.form.testCount, 0)]
-                manualTestData.tableData = [
-                    {name: '移动网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
-                    {name: '联通网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
-                    {name: '电信网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
-                    {name: '域名网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
-                ]
-            }
+            // 获取数据 以及 刷新数据
+            let {manualTestData, refreshManualTestData} = useManualTestDefault()
 
             const onRefresh = () => {
                 setTimeout(() => {
-                    Toast(`线程数: ${manualTestData.form.threadCount}\r\n拨测数: ${manualTestData.form.testCount}\r\n拨测成功`);
+                    refreshManualTestData()
                     loading.value = false;
-                    setEchartsData()
                 }, 1000);
             };
-
-            watch(manualTestData.form, ()=> {
-                setEchartsData()
-            })
 
             return {
                 loading,
---
+++ src/views/dialTest/manualTest/useManualTestDefault.js
@@ -0,0 +1,82 @@
+import {reactive, watch} from 'vue'
+import utils from 'aki_js_utils'
+import {Toast} from 'vant'
+import store from '/@/vuex/vuexValues'// vuex
+
+// 设置初始化数据
+const setDefaultManualTestData = function () {
+    // form
+    const form = {
+        threadCount: 1,
+        testCount: 2
+    }
+    // 图表信息
+    const echartData = {
+        barV: {
+            dataX: [0, 0, 0, 0]
+        },
+        barH: {
+            dataX: [0, 0, 0, 0]
+        },
+    }
+    // 表格
+    const tableData = [
+        {name: '移动网络', max: 0, min: 0, level: '网络通畅'},
+        {name: '联通网络', max: 0, min: 0, level: '网络通畅'},
+        {name: '电信网络', max: 0, min: 0, level: '网络通畅'},
+        {name: '域名网络', max: 0, min: 0, level: '网络通畅'},
+    ]
+    const tmp = {
+        form: form,
+        echartData: echartData,
+        tableData: tableData
+    }
+    const manualTestData = reactive(tmp)
+    return manualTestData
+}
+// 设置测试数据
+const setTestManualTestData = function (manualTestData) {
+    // 图表信息
+    manualTestData.echartData = {
+        barV: {
+            dataX: [utils.randomFlow(0, 30, 0),
+                utils.randomFlow(0, 30, 0),
+                utils.randomFlow(0, 30, 0),
+                utils.randomFlow(0, 30, 0)]
+        },
+        barH: {
+            dataX: [utils.randomFlow(0, 30, 0),
+                utils.randomFlow(0, 30, 0),
+                utils.randomFlow(0, 30, 0),
+                utils.randomFlow(0, 30, 0)]
+        },
+    }
+    // 表格
+    manualTestData.tableData = [
+        {name: '移动网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
+        {name: '联通网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
+        {name: '电信网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
+        {name: '域名网络',max: utils.randomFlow(20, 30, 0), min: utils.randomFlow(0, 20, 0), level: '网络通畅'},
+    ]
+}
+// 设置真正数据
+const setManualTestData = function (manualTestData) {
+    Toast('去后台查吧')
+}
+
+export default function () {
+    const manualTestData = setDefaultManualTestData()
+    // 测试刷新数据
+    const refreshManualTestData = function () {
+        if (store.state.isTest) {
+            setTestManualTestData(manualTestData)
+        } else {
+            setManualTestData(manualTestData)
+        }
+    }
+    // watch
+    watch(manualTestData.form, ()=> {
+        refreshManualTestData()
+    })
+    return {manualTestData, refreshManualTestData}
+}
