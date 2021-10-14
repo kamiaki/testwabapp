e24b41de3d3adec75ba563297f67e59ac3ed067e
aki <247557768@qq.com>
2021-10-14 08:42:14 +0800

    123

--- src/utils/ajax/baseConfig.js
+++ src/utils/ajax/baseConfig.js
@@ -3,6 +3,7 @@
  */
 const getBaseUrl = () => {
   const URL_BASE_DEV = '/test' // 本地调试
+  // const URL_BASE_DEV = 'http://119.3.222.120:8081' // 本地调试
   const URL_BAES_PRO = '/test' // nginx发布
   let baseUrl
   let ENV = process.env.NODE_ENV
