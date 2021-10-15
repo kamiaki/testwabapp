4e2b3b5c002bdaf9197aad6aa15e846ddcff9106
aki <247557768@qq.com>
2021-10-15 16:28:27 +0800

    123

--- src/views/query/queryTest/QueryTestPage.vue
+++ src/views/query/queryTest/QueryTestPage.vue
@@ -43,6 +43,6 @@
         width: 95%;
         border-radius: 5px;
         overflow: hidden;
-        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
     }
 </style>
